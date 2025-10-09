9e369c09b0039a8492887c6b4c45ec8c45491255
Vinod Khare <vinod.khare@gmail.com>
2025-10-08 21:15:02 -0600

    `time_this`: class to compute elapsed time.

---
+++ zenslam_core/include/zenslam/time_this.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <chrono>
+
+class time_this
+{
+public:
+    time_this()                             = delete;
+    time_this(const time_this &)            = delete;
+    time_this(time_this &&)                 = delete;
+    time_this &operator=(const time_this &) = delete;
+    time_this &operator=(time_this &&)      = delete;
+
+    explicit time_this(std::chrono::system_clock::duration &time) :
+        _time { time } {}
+
+    ~time_this()
+    {
+        _time = (std::chrono::system_clock::now() - _start);
+    }
+
+private:
+    std::chrono::time_point<std::chrono::system_clock> _start = std::chrono::system_clock::now();
+    std::chrono::system_clock::duration &              _time;
+};
